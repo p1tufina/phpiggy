179434ee9dfaf18feec5778d5f1b8bd06089d267
Yadira Rodriguez <ymrodrig@gmail.com>
2023-11-03 07:16:51 -0400

    Routing and Template Engines
    Adding code for routing and template engines.

--- src/App/Controllers/AboutController.php
+++ src/App/Controllers/AboutController.php
@@ -7,7 +7,7 @@ namespace App\Controllers;
 use Framework\TemplateEngine;
 use App\Config\Paths;
 
-class HomeController
+class AboutController
 {
   private TemplateEngine $view;
 
@@ -15,8 +15,8 @@ class HomeController
   {
     $this->view = new TemplateEngine(Paths::VIEW);
   }
-  public function home()
+  public function about()
   {
-    echo $this->view->render("/index.php", ['title' => 'Home page']);
+    echo $this->view->render("about.php", ['title' => 'About page']);
   }
 }
