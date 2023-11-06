439c5cca7279b12bc0aaad38f9c25b4cb0267279
Yadira Rodriguez <ymrodrig@gmail.com>
2023-11-06 07:29:33 -0500

    Containers and Dependency Injection
    Completed Containers and Dependency Injection Section

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -7,6 +7,7 @@ namespace Framework;
 class Router
 {
   private array $routes = [];
+  private array $middlewares = [];
 
   public function add(string $method, string $path, array $controller)
   {
@@ -27,7 +28,7 @@ class Router
     return $path;
   }
 
-  public function dispatch(string $path, string $method)
+  public function dispatch(string $path, string $method, Container $container = null)
   {
     $path = $this->normalizePath($path);
     $method = strtoupper($method);
@@ -42,8 +43,27 @@ class Router
 
       [$class, $function] = $route['controller'];
 
-      $controllerInstance = new $class;
-      $controllerInstance->{$function}();
+      $controllerInstance = $container ?
+        $container->resolve($class) :
+        new $class;
+
+      $action = fn () => $controllerInstance->{$function}();
+
+      foreach ($this->middlewares as $middleware) {
+        $middlewareInstance = $container ?
+          $container->resolve($middleware) :
+          new $middleware;
+        $action = fn () => $middlewareInstance->process($action);
+      }
+
+      $action();
+
+      return;
     }
+  }
+
+  public function addMiddleware(string $middleware)
+  {
+    $this->middlewares[] = $middleware;
   }
 }
