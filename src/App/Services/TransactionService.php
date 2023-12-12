efd1077545c2ddd12bc8fee9eb3240fcbf7dfa2a
Yadira Rodriguez <ymrodrig@gmail.com>
2023-12-12 13:37:41 -0500

    completed CRUD section
    COmpleted CRUD section

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -28,20 +28,79 @@ class TransactionService
     );
   }
 
-  public function getUserTransactions(): array
+  public function getUserTransactions(int $length, int $offset): array
   {
     $searchTerm = addcslashes($_GET['s'] ?? '', '%_');
+    $params = [
+      'user_id' => $_SESSION['user'],
+      'description' => "%{$searchTerm}%"
+    ];
+
     $transactions = $this->db->query(
       "SELECT *, DATE_FORMAT(date, '%Y-%m-%d') as formatted_date 
       FROM transactions 
-      WHERE user_id = :user_id
-      AND description LIKE :description",
-      [
-        'user_id' => $_SESSION['user'],
-        'description' => "%{$searchTerm}%"
-      ]
+      WHERE user_id = :user_id 
+      AND description LIKE :description 
+      LIMIT {$length} OFFSET {$offset}",
+      $params
     )->findAll();
 
-    return $transactions;
+    $transactionCount = $this->db->query(
+      "SELECT COUNT(*) 
+      FROM transactions 
+      WHERE user_id = :user_id 
+      AND description LIKE :description",
+      $params
+    )->count();
+
+    return [$transactions, $transactionCount];
+  }
+
+  public function getUserTransaction(string $id)
+  {
+    return $this->db->query(
+      "SELECT *, DATE_FORMAT(date, '%Y-%m-%d') as formatted_date
+      FROM transactions 
+      WHERE id = :id 
+      AND user_id = :user_id",
+      [
+        'id' => $id,
+        'user_id' => $_SESSION['user']
+      ]
+    )->find();
+  }
+
+  public function update(array $formData, int $id)
+  {
+    $formattedDate = "{$formData['date']} 00:00:00";
+
+    $this->db->query(
+      "UPDATE transactions 
+      SET description = :description, 
+        amount = :amount, 
+        date = :date
+      WHERE id = :id
+      AND user_id = :user_id",
+      [
+        'description' => $formData['description'],
+        'amount' => $formData['amount'],
+        'date' => $formattedDate,
+        'id' => $id,
+        'user_id' => $_SESSION['user']
+      ]
+    );
+  }
+
+  public function delete(int $id)
+  {
+    $this->db->query(
+      "DELETE FROM transactions 
+      WHERE id = :id
+      AND user_id = :user_id",
+      [
+        'id' => $id,
+        'user_id' => $_SESSION['user']
+      ]
+    );
   }
 }
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -12,11 +12,15 @@ class Router
   public function add(string $method, string $path, array $controller)
   {
     $path = $this->normalizePath($path);
+
+    $regexPath = preg_replace('#{[^/]+}#', '([^/]+)', $path);
+
     $this->routes[] = [
       'path' => $path,
       'method' => strtoupper($method),
       'controller' => $controller,
-      'middlewares' => []
+      'middlewares' => [],
+      'regexPath' => $regexPath
     ];
   }
 
@@ -32,15 +36,23 @@ class Router
   public function dispatch(string $path, string $method, Container $container = null)
   {
     $path = $this->normalizePath($path);
-    $method = strtoupper($method);
+    $method = strtoupper($_POST['_METHOD'] ?? $method);
 
     foreach ($this->routes as $route) {
       if (
-        !preg_match("#^{$route['path']}$#", $path) ||
+        !preg_match("#^{$route['regexPath']}$#", $path, $paramValues) ||
         $route['method'] !== $method
       ) {
         continue;
       }
+
+      array_shift($paramValues);
+
+      preg_match_all('#{([^/]+)}#', $route['path'], $paramKeys);
+
+      $paramKeys = $paramKeys[1];
+
+      $params = array_combine($paramKeys, $paramValues);
 
       [$class, $function] = $route['controller'];
 
@@ -48,7 +60,7 @@ class Router
         $container->resolve($class) :
         new $class;
 
-      $action = fn () => $controllerInstance->{$function}();
+      $action = fn () => $controllerInstance->{$function}($params);
 
       $allMiddleware = [...$route['middlewares'], ...$this->middlewares];
 
